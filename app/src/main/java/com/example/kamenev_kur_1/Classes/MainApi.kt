457cf3ac7ec186f8958ba42d2ae77e0479657a6f
alastor046 <turtlewithrabbitears@yandex.ru>
2024-04-12 19:50:50 +0300

    testCommit

---
+++ app/src/main/java/com/example/kamenev_kur_1/Classes/MainApi.kt
@@ -0,0 +1,17 @@
+package com.example.kamenev_kur_1.Classes
+
+
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface MainApi {
+
+    @POST("Users/userAuth")
+    suspend fun postLoginData(@Body authData: AuthData): Users
+
+    @POST("Users")
+    suspend fun regUser(@Body users: Users) : Response<Users>
+}
